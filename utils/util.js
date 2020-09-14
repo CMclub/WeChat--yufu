2c27201ae4d7f6db743336d11c861431fa773faa
Eric <48383124+eric-marcus@users.noreply.github.com>
2020-09-14 20:16:28 +0800

    a-utils

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,5 @@
+const app = getApp()
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -15,5 +17,41 @@ const formatNumber = n => {
 }
 
 module.exports = {
-  formatTime: formatTime
+  reToken: reToken
+}
+
+function reToken(){
+  var _this = this
+  //获取用户openid和token
+  wx.login({
+    success(res) {
+      if (res.code) {
+          //获取用户openid
+          wx.request({
+              url: 'https://www.shoolos.cn:8000/api/user/',
+              data: {
+                  code: res.code
+              },
+              header: {
+                  'content-type': 'application/json',
+              },
+              success(res) {
+                  var obj = JSON.parse(res.data.openid)
+                  app.globalData.openid = obj.openid  //设置openid , 全局变量
+                  app.globalData.token = res.data.token  //设置token , 全局变量
+                  app.globalData.root = res.data.root
+                  console.log('openid:  '+app.globalData.openid)
+                  console.log('token:   '+app.globalData.token)
+                  console.log('root:  '+app.globalData.root)
+              },
+              fail(res){
+                console.log(res)
+              }
+          })
+      } else {
+          console.log('登录失败！' + res.errMsg)
+      }
+    }
+  })
+  return 1;
 }
